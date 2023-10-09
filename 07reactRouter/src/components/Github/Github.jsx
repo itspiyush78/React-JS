1d600527339185d2930c02e99196ad7f81eaa6e6
itspiyush78 <itspiyush78@gmail.com>
2023-10-09 12:46:34 +0530

    react router done

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Github(){
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-xl'>Github followers:</div>
+    )
+}
+
+export default Github
