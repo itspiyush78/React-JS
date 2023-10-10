88f5d197e8d90bb6106eefdd2aa60c58f62f139d
itspiyush78 <itspiyush78@gmail.com>
2023-10-10 17:59:30 +0530

    theme switcher deploy

---
+++ 09themeswitcher/src/components/Card.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+<div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<a href="/">
+<img className="p-8 rounded-t-lg" src="https://lh3.googleusercontent.com/pw/ADCreHcwvo75R8-z5bP0vtqWBcSkZLzjil1V2dd9NvYn7aEIsdBFfRlS_p4_3ZwDCH1lyE4mYV6WIGmRcmZ9FgFKmy28szErRVuvTQ59rCFr7QY4loDRFJYf2q9IjX-lh6OXQtJu9K2ugNAZjLVGAQyu67yGXJ0TmdnZ0SA1BXFbqsq0SY6R-zYm-DAzuJIk9dLIiP2jlLdy1l4ONSROSPDXca6LrK_thW7ja94sp42BSAOw53WJMAVZjrrGEaVsoHDMzfN6ravy_vXwG7tQUtdmy4aHxuuSEP319ByR0bTlj2mi2g8rLkfi_uv0zXdPR-Fi3nQWyPW1DcL7lEDR6O-HoIEi6keGrGxQV4SPBOpwqBMZUIpMEWYqPZvI3wHxcinEu3xNpoG-gFvY3C0XDERZN0aInQYNhr76sz8w-DPKL4mI0gRtKsAQYYPepbtFE25GVp7IVG9S-XGfqqIXZLcJyDHZXg1w2NkYrTiCPmGn8xVMmtOCyTqWIbl7LMAv5EttcJveXd92DBwOkB8bIgMrzJ_49YG83muwqqmAI2UBpLJXaOjooJrdBBAys2DuzoIkZ4rEVXFDBuuhz0Fm_wOxGysa3210Qm3s6yqRc2-IXw2UX2VxV6gUW32yUTJccP4zp7q43yEKLWvZZBZP57MX346aYBNpZIlMz7VakcdpiJtE0cO3IWiN3cYGPBla0s8o0DlOB0KdhVgwPPAgM_FZu1Fm2VP88gdu5daScAoKPzt01sUooiWSQfxcdMlD-woy9VfpXm5dAiilaIWqCvUImB3e-mJJQxRtKVKk_E3Oiy-oRNEfWGZhY3j4zIsMDvT2Fhkk3I-LwlNXCh8nYOMS3qF0bIB60W3iB7YH2XHe18nV_j-_WGiC6kFjjoEGWH-Vn7SYWPPoYN6gQz6nx1h_utHKNA=w517-h689-s-no?authuser=0" alt="product_image1" />
+</a>
+<div className="px-5 pb-5">
+<a href="/">
+    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        HP proBook 440 G5, Premium Laptop Intel 8th Gen     </h5>
+</a>
+<div className="flex items-center mt-2.5 mb-5">
+    <svg
+        className="w-4 h-4 text-yellow-300 mr-1"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+    >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+    <svg
+        className="w-4 h-4 text-yellow-300 mr-1"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+    >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+    <svg
+        className="w-4 h-4 text-yellow-300 mr-1"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+    >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+    <svg
+        className="w-4 h-4 text-yellow-300 mr-1"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+    >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+    <svg
+        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+    >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+    </svg>
+    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+        4.0
+    </span>
+</div>
+<div className="flex items-center justify-between">
+    <span className="text-3xl font-bold text-gray-900 dark:text-white">$721</span>
+    <a
+        href="/"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+        Add to cart
+    </a>
+</div>
+</div>
+</div>
+    );
+}
