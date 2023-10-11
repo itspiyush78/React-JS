eae6aaba348a46f33fd76def8a1da152ca4eec76
itspiyush78 <itspiyush78@gmail.com>
2023-10-11 08:06:11 +0530

    update

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                   <ThemeBtn />
-              </div>
+             </div> 
 
               <div className="w-full max-w-sm mx-auto">
                   <Card />
