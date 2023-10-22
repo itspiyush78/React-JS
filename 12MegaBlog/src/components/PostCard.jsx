1d769288080fff1c84cc7f16cf12c0132eaef5f0
itspiyush78 <itspiyush78@gmail.com>
2023-10-22 22:09:02 +0530

    login under process

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -8,8 +8,11 @@ function PostCard({$id, title, featuredImage}) {
        <Link to={`/post/${$id}`}>
           <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'></div>
-            <img src={appwriteService.getFilePreview()} alt="" />
+            <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+            className='rounded-xl' />
+
           </div>
+          <h2 className='text-xl font-bold'>{title}</h2>
        </Link>
     )
 }
